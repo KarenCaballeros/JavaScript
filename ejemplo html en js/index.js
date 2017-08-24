c60c5712f5501002f4d2df155165d9194a6acec0
KarenCaballeros <anakarencaballeros@gmail.com>
2017-08-24 11:36:45 -0600

    agregue posicion

--- ejemplo html en js/index.js
+++ ejemplo html en js/index.js
@@ -1,14 +1,26 @@
-function App(){
+function Cuadradocirculo(position){
+
+	//titulo
 	var h1Titulo= document.createElement("h1");
 	var titulo = document.createTextNode("ejercicio");
 	h1Titulo.appendChild(titulo);
 
+	//creacioncuadrado
 	var divCuadro= document.createElement("div");
 	divCuadro.className = "cuadro";
+
+	//cracion de circulo
 	var divCirculo= document.createElement("div");
 	divCirculo.className = "circulo";
+	//envio posicion 
+	divCirculo.style.left = position.x + "px";
+	divCirculo.style.top= position.y + "px";
+
+
+	//agrego circulo a cuadrado
 	divCuadro.appendChild(divCirculo);
 
+	//creacion de div
 	var app= document.createElement("div");
 	app.className= "app";
 	app.appendChild(h1Titulo);
@@ -17,11 +29,11 @@ function App(){
 	return app;
 
 }
-function render(mountPoint, component){
-	mountPoint.appendChild(component());
+function render(mountPoint, component, state){
+	mountPoint.appendChild(component(state));
 }
 
 
 var root= document.getElementById("root");
-render(root,App);
+render(root, Cuadradocirculo, {x: 10, y:20});
 	
